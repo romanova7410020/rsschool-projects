efd243fbe78eb59f61fc28c01989ab4edb6e665d
RomanovaNasstasia <romanova7410020@gmail.com>
2025-09-28 20:49:23 +0300

    :feat: add burger menu

---
+++ portfolio/script.js
@@ -0,0 +1,22 @@
+const burgerMenu = document.querySelector(".burger");
+const navigation = document.querySelector('.navigation');
+const body = document.body;
+const menuLinks = document.querySelectorAll('.navigation a');
+
+if (burgerMenu) {
+  burgerMenu.addEventListener('click', function(e){
+    burgerMenu.classList.toggle('_active');
+    if (navigation) {
+      navigation.classList.toggle('navigation_active');
+    }
+    body.classList.toggle('body-block');
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      burgerMenu.classList.remove('_active');
+      navigation.classList.remove('navigation_active');
+      body.classList.remove('body-block');
+    });
+  });
+}
