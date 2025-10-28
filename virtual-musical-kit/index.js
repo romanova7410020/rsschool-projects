64c98b738ed3aa9fe28ce3c374aabc6775c834d5
RomanovaNasstasia <romanova7410020@gmail.com>
2025-10-29 00:47:42 +0300

    fix: add canvas with some circles

--- virtual-musical-kit/index.js
+++ virtual-musical-kit/index.js
@@ -122,7 +122,8 @@ keyWhiteClasses.forEach(className => {
 
    keyDiv.addEventListener('mouseleave', () =>{
     keyDiv.style.background = 'white';
-    
+    sound.pause();
+    sound.currentTime = 0;
   });
   });
 });
@@ -159,6 +160,121 @@ if (keyWhiteClasses.includes(className)) {
     }
   }
 });
+
+const canvas = document.createElement('canvas');
+canvas.classList.add('canvas');
+document.body.appendChild(canvas);
+
+const dpr = window.devicePixelRatio || 1;
+const rect = canvas.getBoundingClientRect();
+canvas.width = rect.width * dpr;
+canvas.height = rect.height * dpr;
+const ctx = canvas.getContext('2d');
+
+const canvasWrap = createElement({
+  classes: ['canvas__wrap'],
+  tag: 'section',
+  parent: canvas,
+});
+
+let mouse = {
+   x: undefined,
+   y: undefined,
+}
+let maxRadius = 100;
+let minRadius = 30;
+
+window.addEventListener('mousemove', function(event) {
+  mouse.x = (event.clientX - rect.left) * (canvas.width / rect.width);
+  mouse.y = ( event.clientY - rect.top) * (canvas.height / rect.height);
+})
+
+function Circle(x, y, dx, dy, radius, type) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.type = type;
+
+  this.draw = function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+
+     let grad;
+    if (this.type === 1) {
+      grad = ctx.createRadialGradient(this.x, this.y, 5, this.x, this.y, this.radius);
+      grad.addColorStop(0, 'rgb(35, 151, 247)');
+      grad.addColorStop(1, 'rgb(248, 208, 30)');
+    } else if (this.type === 2) {
+      grad = ctx.createRadialGradient(this.x, this.y, 5, this.x, this.y, this.radius);
+      grad.addColorStop(0, 'rgb(236, 200, 40)');
+      grad.addColorStop(1, 'rgb(50, 149, 230)');
+    }
+
+    ctx.fillStyle = grad;
+    ctx.fill();
+    ctx.strokeStyle = this.type === 1 ? 'rgb(248, 208, 30)' : 'rgb(50, 149, 230)';
+    ctx.lineWidth = 1;
+    ctx.stroke();
+  }
+
+  this.update = function() {
+    if (this.x + this.radius > canvas.width /dpr ||
+        this.x - this.radius < 0) {
+        this.dx = - this.dx;
+  }
+  if (this.y + this.radius > canvas.height / dpr ||
+      this.y - this.radius < 0) {
+      this.dy = - this.dy;
+  }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if (
+  mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+  mouse.y - this.y < 50 && mouse.y - this.y > -50
+) {
+  if (this.radius < maxRadius) {
+    this.radius += 1;
+  }
+} else {
+  if (this.radius > minRadius) {
+    this.radius -= 1;
+  }
+}
+
+    this.draw();
+  }
+
+}
+let circleArray = [];
+
+for (let i = 0; i < 4; i++) {
+  let radius = 30;
+  let x = Math.random() * (canvas.width / dpr - radius * 2) + radius;
+  let dx = (Math.random() - 0.5) * 8;
+  let y = Math.random() * (canvas.height / dpr - radius * 2) + radius;
+  let dy = (Math.random() - 0.5) * 8;
+  let type = i % 2 === 0 ? 1 : 2;
+  circleArray.push(new Circle(x, y, dx,dy,radius, type));
+}
+
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+
+
+  for( let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+
+}
+
+animate();
 
 
 
